364f4004989696c15197b1622d325e875332124d
Yangaliev Asaf <asafyang@mail.ru>
2023-11-11 02:28:24 +0500

    Added new algorithm

--- src/main/java/uz/yangaliev/yandex_solutions/YandexTask5Solution.java
+++ src/main/java/uz/yangaliev/yandex_solutions/YandexTask5Solution.java
@@ -3,7 +3,6 @@ package uz.yangaliev.yandex_solutions;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.function.Function;
 
 public class YandexTask5Solution {
@@ -36,19 +35,19 @@ public class YandexTask5Solution {
             int j = any.length() - i - 1;
             Function<String, Byte> function = (a) -> (byte) ((byte) a.charAt(j) - ZERO_INDEX);
             innerBitwiseSorting(array, function);
-            for (int bucketNumber = 0, current = 0; bucketNumber < array.length; bucketNumber++) {
+            for (int bucketNumber = 0, current = 0; bucketNumber < 10; bucketNumber++) {
                 pw.print(String.format("Bucket %d: ", bucketNumber));
-                if (function.apply(array[current]) != bucketNumber) {
+                if (current >= array.length || function.apply(array[current]) != bucketNumber) {
                     pw.println("empty");
                 } else {
+                    int begin = current;
                     while (current < array.length && function.apply(array[current]) == bucketNumber) {
-                        pw.print(array[current]);
-                        pw.print(" ");
                         current++;
                     }
-                    pw.println();
+                    printArray(pw, array, begin, current - 1);
                 }
             }
+            printEnd(pw);
         }
     }
 
@@ -77,7 +76,7 @@ public class YandexTask5Solution {
     private static void beginPrint(PrintWriter pw, String[] array) {
         pw.println("Initial array:");
         printArray(pw, array);
-        pw.println("**********");
+        printEnd(pw);
     }
 
     private static void endPrint(PrintWriter pw, String[] array) {
@@ -91,5 +90,17 @@ public class YandexTask5Solution {
             pw.print(", ");
         }
         pw.println(array[array.length - 1]);
+    }
+
+    private static void printArray(PrintWriter pw, String[] array, int begin, int end) {
+        for (int i = begin; i < end; i++) {
+            pw.print(array[i]);
+            pw.print(", ");
+        }
+        pw.println(array[end]);
+    }
+
+    private static void printEnd(PrintWriter pw) {
+        pw.println("**********");
     }
 }
